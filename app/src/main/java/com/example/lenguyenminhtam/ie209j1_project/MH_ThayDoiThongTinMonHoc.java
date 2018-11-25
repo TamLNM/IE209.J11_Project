f5732048fa4e929759e835c79f147a63819ff3b0
TamLNM <minhtam2996@gmail,com>
2018-11-25 18:36:37 +0700

    [TamLNM] Fix bugs Setting Screens: ThayDoiQuyDinh, ThayDoiThongTinMonHoc

--- app/src/main/java/com/example/lenguyenminhtam/ie209j1_project/MH_ThayDoiThongTinMonHoc.java
+++ app/src/main/java/com/example/lenguyenminhtam/ie209j1_project/MH_ThayDoiThongTinMonHoc.java
@@ -1,7 +1,12 @@
 package com.example.lenguyenminhtam.ie209j1_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MH_ThayDoiThongTinMonHoc extends AppCompatActivity {
 
@@ -12,6 +17,46 @@ public class MH_ThayDoiThongTinMonHoc extends AppCompatActivity {
         // Load fragment
         Fragment_MonHoc fm_monhoc = new Fragment_MonHoc();
         loadFragment(fm_monhoc);
+
+        // Add bottom navigation bar
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(4);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Intent intent0 = new Intent(MH_ThayDoiThongTinMonHoc.this, MH_DanhSachLop.class);
+                        startActivity(intent0);
+                        break;
+
+                    case R.id.nav_themsv:
+                        Intent intent1 = new Intent(MH_ThayDoiThongTinMonHoc.this, ThemHocSinhActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.nav_nhapdiem:
+                        Intent intent2 = new Intent(MH_ThayDoiThongTinMonHoc.this, NhapDiemActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.nav_baocao:
+                        Intent intent3 = new Intent(MH_ThayDoiThongTinMonHoc.this, BaoCaoActivity.class);
+                        startActivity(intent3);
+                        break;
+
+                    case R.id.nav_setting:
+
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 
     private boolean loadFragment(Fragment_MonHoc fragment) {
